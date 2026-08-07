b93c730b62a440abd73e0692636e837ac3737774
Dennisbed1234 <dennisbed1234@gmail.com>
2026-08-06 23:48:35 -0400

    Update logger.ts

--- fictional-bank/backend/src/config/logger.ts
+++ fictional-bank/backend/src/config/logger.ts
@@ -10,9 +10,11 @@ export const logger = winston.createLogger({
   defaultMeta: { service: "fictional-bank-api" },
   transports: [
     new winston.transports.Console({
-      format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+      level: "info",
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+      ),
     }),
-    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
-    new winston.transports.File({ filename: "logs/combined.log" }),
   ],
 });
